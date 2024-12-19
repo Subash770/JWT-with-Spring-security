4dd5644f020a7e8d440b79c2aaac9887861416f0
Subash <spidybeatz25@gmail.com>
2024-12-19 17:19:57 +0530

    update-RBAC

---
+++ auth-api/src/main/java/com/project/auth_api/entity/RoleEnum.java
@@ -0,0 +1,7 @@
+package com.project.auth_api.entity;
+
+public enum RoleEnum {
+    USER,
+    ADMIN,
+    SUPER_ADMIN
+}
--- auth-api/src/main/java/com/project/auth_api/service/UserService.java
+++ auth-api/src/main/java/com/project/auth_api/service/UserService.java
@@ -1,18 +1,29 @@
 package com.project.auth_api.service;
 
+import com.project.auth_api.dto.RegisterUserDto;
+import com.project.auth_api.entity.Role;
+import com.project.auth_api.entity.RoleEnum;
 import com.project.auth_api.entity.User;
+import com.project.auth_api.repository.RoleRepository;
 import com.project.auth_api.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> allUsers() {
@@ -21,5 +32,21 @@ public class UserService {
         userRepository.findAll().forEach(users::add);
 
         return users;
+    }
+
+    public User createAdministrator(RegisterUserDto input) {
+        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.ADMIN);
+
+        if (optionalRole.isEmpty()) {
+            return null;
+        }
+
+        var user = new User()
+                .setFullName(input.getFullName())
+                .setEmail(input.getEmail())
+                .setPassword(passwordEncoder.encode(input.getPassword()))
+                .setRole(optionalRole.get());
+
+        return userRepository.save(user);
     }
 }
